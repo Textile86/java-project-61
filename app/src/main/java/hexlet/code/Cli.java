ca9fee114b9bd5435e0b9f21d6bcf9e501b363ef
textile86 <textile2006@list.ru>
2025-07-14 12:11:26 +0300

    add Makefile add greetings

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,9 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    public static String getUserName() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine() ;
+    }
+}
